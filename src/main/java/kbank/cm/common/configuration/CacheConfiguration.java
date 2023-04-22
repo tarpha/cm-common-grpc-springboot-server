29de034f4c71d6d4c24e12be44f5eab3dac652d3
LEE JEONG WOOK <tarpha@gmail.com>
2023-04-22 23:58:44 +0900

    set jedis

--- src/main/java/kbank/cm/common/configuration/CacheConfiguration.java
+++ src/main/java/kbank/cm/common/configuration/CacheConfiguration.java
@@ -1,24 +1,67 @@
 package kbank.cm.common.configuration;
 
+import java.time.Duration;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.cache.interceptor.KeyGenerator;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.cache.CacheKeyPrefix;
+import org.springframework.data.redis.cache.RedisCacheConfiguration;
+import org.springframework.data.redis.cache.RedisCacheManager;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
+import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.RedisSerializationContext;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 @EnableCaching
 @Configuration
 public class CacheConfiguration {
-    // @Bean
-    // public CacheManager cacheManager() {
-    //     SimpleCacheManager cacheManager = new SimpleCacheManager();
-    //     Cache booksCache = new ConcurrentMapCache("grpc");
-    //     cacheManager.setCaches(Arrays.asList(booksCache));
-    //     return cacheManager;
-    // }
+
+    @Value("${spring.data.redis.host}")
+    private String hostName;
+
+    @Value("${spring.data.redis.port}")
+    private int port;
   
-    @Bean("hashKeyGenerator")
-    public KeyGenerator keyGenerator() {
+    @Bean
+    public KeyGenerator hashKeyGenerator() {
         return new HashKeyGenerator();
     }
-    
+
+    @Bean
+    public JedisConnectionFactory jedisConnectionFactory() {
+        return new JedisConnectionFactory(new RedisStandaloneConfiguration(hostName, port));
+    }
+
+    @Bean
+    public RedisTemplate<String, Object> redisTemplate() {
+        RedisTemplate<String, Object> template = new RedisTemplate<>();
+        template.setConnectionFactory(jedisConnectionFactory());
+        return template;
+    }
+
+    @Bean
+    public RedisCacheManager cacheManager(RedisConnectionFactory connectionFactory) {
+        RedisCacheConfiguration configuration = RedisCacheConfiguration.defaultCacheConfig()
+                .disableCachingNullValues()
+                .entryTtl(Duration.ofMinutes(60))
+                .computePrefixWith(CacheKeyPrefix.simple())
+                .serializeKeysWith(RedisSerializationContext.SerializationPair.fromSerializer(new StringRedisSerializer())); // redis 캐시 데이터 저장방식을 StringSeriallizer로 지정
+
+        Map<String, RedisCacheConfiguration> grpcCacheConfigurations = new HashMap<>();
+        grpcCacheConfigurations.put("grpc:encrypt", RedisCacheConfiguration.defaultCacheConfig()
+            .entryTtl(Duration.ofMinutes(10)));
+        grpcCacheConfigurations.put("grpc:decrypt", RedisCacheConfiguration.defaultCacheConfig()
+            .entryTtl(Duration.ofMinutes(5)));
+        
+
+        return RedisCacheManager.RedisCacheManagerBuilder.fromConnectionFactory(connectionFactory).cacheDefaults(configuration)
+                .withInitialCacheConfigurations(grpcCacheConfigurations).build();
+    }
 }
