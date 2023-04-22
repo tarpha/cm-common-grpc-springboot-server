2f03e48fdb54e648c02dbdf5ab20fce751bbe23b
LEE JEONG WOOK <tarpha@gmail.com>
2023-04-22 16:08:31 +0900

    cache

--- src/main/java/kbank/cm/common/service/EncryptService.java
+++ src/main/java/kbank/cm/common/service/EncryptService.java
@@ -1,7 +1,6 @@
 package kbank.cm.common.service;
 
 import org.apache.commons.codec.binary.Base64;
-import org.springframework.cache.annotation.CacheConfig;
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 
@@ -9,19 +8,20 @@ import lombok.extern.slf4j.Slf4j;
 
 @Service
 @Slf4j
-@CacheConfig(cacheNames={"grpc"})
 public class EncryptService {
 
-    @Cacheable(value = "grpc:encrypt", keyGenerator = "hashKeyGenerator")
+    @Cacheable(value = "grpc:encrypt", keyGenerator = "hashKeyGenerator", unless="#result.startsWith('ERROR')")
     public String encrypt(String plain) {
         log.info("encrypt called");
         return Base64.encodeBase64String(plain.getBytes());
+        // return "ERROR OCCURS";
     }
 
-    @Cacheable(value = "grpc:decrypt", keyGenerator = "hashKeyGenerator")
+    @Cacheable(value = "grpc:decrypt", keyGenerator = "hashKeyGenerator", unless="#result.startsWith('ERROR')")
     public String decrypt(String encrypt) {
         log.info("decrypt called");
         return new String(Base64.decodeBase64(encrypt));
+        // return "ERROR OCCURS";
     }
     
 }
